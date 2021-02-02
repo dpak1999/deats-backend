0992b0a1838088fb8ff81e98cf260ae872bcc933
dpak1999 <deepakkumardash313@gmail.com>
2021-02-02 23:53:10 +0530

    add : jwt interfaces

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,7 +38,9 @@ import { JwtModule } from './jwt/jwt.module';
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    JwtModule.forRoot(),
+    JwtModule.forRoot({
+      privateKey: process.env.SECRET_KEY,
+    }),
     UsersModule,
     CommonModule,
   ],
