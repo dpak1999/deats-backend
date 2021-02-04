e969f5254f205964719a263c5454c228aee828d6
dpak1999 <deepakkumardash313@gmail.com>
2021-02-04 23:28:16 +0530

    add : auth module and adding guards

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
+import { AuthGuard } from 'src/auth/auth.guard';
 import {
   CreateAccountInput,
   CreateAccountOutput,
@@ -49,11 +51,6 @@ export class UsersResolver {
   }
 
   @Query(() => User)
-  me(@Context() context) {
-    if (!context.user) {
-      return;
-    } else {
-      return context.user;
-    }
-  }
+  @UseGuards(AuthGuard)
+  me(@Context() context) {}
 }
