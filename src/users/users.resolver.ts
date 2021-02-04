bd26ada7f57021ef56557313576b83e8fc755089
dpak1999 <deepakkumardash313@gmail.com>
2021-02-04 22:51:59 +0530

    extracting user data using graphql context

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import {
   CreateAccountInput,
   CreateAccountOutput,
@@ -49,5 +49,11 @@ export class UsersResolver {
   }
 
   @Query(() => User)
-  me() {}
+  me(@Context() context) {
+    if (!context.user) {
+      return;
+    } else {
+      return context.user;
+    }
+  }
 }
