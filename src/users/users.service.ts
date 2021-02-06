05797790816bd30dfd9a3d5d9dcd84473940a74d
dpak1999 <deepakkumardash313@gmail.com>
2021-02-06 23:11:16 +0530

    add : verification entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,11 +6,14 @@ import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { EditProfileInput } from './dtos/edit-profile.dto';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -24,7 +27,14 @@ export class UsersService {
       if (exists) {
         return [false, 'There is user with that email'];
       }
-      await this.users.save(this.users.create({ email, password, role }));
+      const user = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verifications.save(
+        this.verifications.create({
+          user,
+        }),
+      );
       return [true];
     } catch (error) {
       return [false, 'Uh Oh! Something went wrong'];
@@ -68,6 +78,8 @@ export class UsersService {
     const user = await this.users.findOne(userId);
     if (email) {
       user.email = email;
+      user.verified = false;
+      await this, this.verifications.save(this.verifications.create({ user }));
     }
     if (password) {
       user.password = password;
