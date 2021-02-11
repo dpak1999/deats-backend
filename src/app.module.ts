a177e868944693e3eded0cd4dbc273a83487be0d
dpak1999 <deepakkumardash313@gmail.com>
2021-02-11 22:01:13 +0530

    add : test config for end-2-end testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { MailModule } from './mail/mail.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -41,7 +41,8 @@ import { MailModule } from './mail/mail.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV !== 'prod',
+      synchronize:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
       logging: true,
       entities: [User, Verification],
     }),
